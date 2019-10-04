5b8530c497666c0cdabf3957fcaed259fa244083
chuck19-20 <chengyu920420@163.com>
2019-10-04 23:04:24 +0100

    this is the git practice

---
+++ Solution.java
@@ -0,0 +1,40 @@
+
+// * Definition for a binary tree node.
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int max = Integer.MIN_VALUE;
+        int maxindex = 0;
+        // int[] left = new int[nums.length - 1];
+        // int[] right = new int[nums.length - 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                maxindex = i;
+            }
+        }
+        TreeNode node = new TreeNode(max);
+
+        int[] left = new int[maxindex];
+        for (int i = 0; i < maxindex; i++) {
+            left[i] = nums[i];
+        }
+        node.left = constructMaximumBinaryTree(left);
+
+        int[] right = new int[nums.length - 1 - maxindex];
+        for (int i = maxindex + 1; i < nums.length; i++) {
+            right[i] = nums[i];
+        }
+        node.right = constructMaximumBinaryTree(right);
+        return node;
+    }
+}
---
+++ TestLeetcode.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class TestLeetcode {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] input = new int[]{3, 2, 1, 6, 0, 5};
+
+        System.out.println(sol.constructMaximumBinaryTree(input));
+    }
+}

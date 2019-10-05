85c78f9e9bbe9f902043d12c120055deec2309b6
chuck19-20 <chengyu920420@163.com>
2019-10-05 14:50:16 +0100

    this is Arrays.sort() && -> practice

--- TestLeetcode.java
+++ TestLeetcode.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class TestLeetcode {
     public static void main(String[] args) {
@@ -15,8 +16,10 @@ public class TestLeetcode {
 //        for (int i = 0; i < input.length; i++) {
 //            System.out.println(Arrays.toString(input[i]));
 //        }
-        Integer[] arr = new Integer[]{0 ,2, 7, 6, 1, 4};
-        Arrays.sort(arr, Collections.reverseOrder());
+//        Integer[] arr = new Integer[]{0 ,2, 7, 6, 1, 4};
+//        int[] arr = new int[]{ 0, 5, 2, 3, 1};
+        String[] arr = new String[]{"Abcd", "1a", "bbc"};
+        Arrays.sort(arr, (a,b) -> (a.length() - b.length()));
         System.out.println(Arrays.toString(arr));
 //        System.out.print(Integer.compare(2,1));
 
